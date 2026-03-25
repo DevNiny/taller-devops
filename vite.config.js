174ae4a40f28380b98a714c7f098555e33070f6a
iamstivgo <iamstivgo@gmail.com>
2026-03-24 20:48:00 -0500

    fix: update repository name comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ Cambia 'mi-app-devops' por el nombre de TU repositorio
-  base: '/taller-devops',
+  // ⚠️ Cambia por el nombre de TU repositorio en GitHub
+  base: '/taller-devops/',
   test: {
     globals: true,
     environment: 'jsdom',
